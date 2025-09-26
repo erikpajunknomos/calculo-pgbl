c15e0ddef845fcb0eb740f3ebdb239e4e0a081d8
erikpajunknomos <erik.pajunk@nomos.com.br>
2025-09-26 15:58:56 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: { extend: {} },
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        nomos: {
+          ink: "#021e19",    // verde escuro
+          lime: "#c8e05b",   // verde lima
+          paper: "#f4ece6",  // off-white
+          gray: "#a6a797"    // cinza
+        },
+      },
+      borderRadius: {
+        "2xl": "1rem"
+      }
+    },
+  },
   plugins: [],
 };
